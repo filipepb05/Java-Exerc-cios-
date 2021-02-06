a430a80015e9b29da8226c83b9ebf33dd35e78a3
filipepb05 <filipepb05@gmail.com>
2021-02-06 18:15:49 -0300

    Exercicio 1

---
+++ projeto-1/src/Aplicação.java
@@ -0,0 +1,9 @@
+public class Aplicação {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        int resultadoSoma1 = calculadora.soma(1,2);
+
+        System.out.println(resultadoSoma1);
+    }
+
+}
--- projeto-1/src/Main.java
+++ projeto-1/src/Main.java
@@ -1,8 +1,17 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Ola Mundo");
 
+        int primeirovalor = 3;
+        int valor2 = 6;
+        int mult = primeirovalor * valor2;
+        int resto = valor2 % primeirovalor;
+        // boolean comp = valor2 > valor1;
+
+         System.out.println(primeirovalor + " multiplicado por " + valor2 + " eh igual a " + mult);
+         System.out.println("O resto da divisão de " + valor2 + " por " + primeirovalor + " é " + resto);
+         // System.out.println(valor2 + " é maior que " + valor1 + "? " + comp);
+        System.out.println(valor2 + " é maior que " + primeirovalor + "? " + (valor2 > primeirovalor));
 
 
     }
